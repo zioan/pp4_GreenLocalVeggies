12cd58de98c39c4b88c1aadcefe619dfb33f0e74
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-19 17:00:29 +0000

    Add Product model and register to admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock')
+    search_fields = ['name', 'category', 'price']
+    list_filter = ['category']
+
+
+admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('Fruit', 'Fruit'),
+        ('Vegetable', 'Vegetable'),
+    ]
+
+    UNIT_CHOICES = [
+        ('Kg', 'Kg'),
+        ('g', 'g'),
+        ('unit', 'unit'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=10, choices=UNIT_CHOICES)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
