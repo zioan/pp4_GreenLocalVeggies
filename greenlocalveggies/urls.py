d4694dcfd986ff0c476668842cb3ae095ef1ef77
zioan <zaharia.ioan@gmail.com>
2024-07-19 10:56:38 +0200

    Testing static files collection

--- greenlocalveggies/urls.py
+++ greenlocalveggies/urls.py
@@ -23,7 +23,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("shop.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
