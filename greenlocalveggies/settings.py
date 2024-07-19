11634b6283ef2a9be2c49a4be1516e6e5f5b1906
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-19 09:24:21 +0000

    Test deployment static files

--- greenlocalveggies/settings.py
+++ greenlocalveggies/settings.py
@@ -130,11 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-#
+# Make static files accessible
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
