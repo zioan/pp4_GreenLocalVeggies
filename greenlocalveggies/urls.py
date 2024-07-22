674b303ee91a401c1dc1060ba2e626000ec6d637
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-22 11:09:59 +0000

    Handle global 404 and improve product view 404 logic

--- greenlocalveggies/urls.py
+++ greenlocalveggies/urls.py
@@ -19,8 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import handler404
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("shop.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'shop.views.custom_404_view'
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.http import Http404
 from .models import Product
 
 # Create your views here.
@@ -10,7 +11,13 @@ def index(request):
 
 
 def product_details(request, product_slug):
-    product = get_object_or_404(Product, slug=product_slug)
-    return render(request, "shop/product-details.html", {
-        "product": product
-    })
+    try:
+        product = Product.objects.get(slug=product_slug)
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+
+    return render(request, "shop/product-details.html", {"product": product})
+
+
+def custom_404_view(request, exception):
+    return render(request, 'shop/404.html', status=404)
