c6d4591cb7356b5a9d6fdb5d1426dd157b88feb3
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-25 13:36:28 +0000

    Add django built-in logout view to urls

--- greenlocalveggies/urls.py
+++ greenlocalveggies/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("customers/", include('customer.urls')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path("", include("shop.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
