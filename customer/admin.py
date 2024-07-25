b90befa239621a122d565bf3141ab8efddf1c3d4
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-25 08:14:24 +0000

    Register customer/user in admin dashboard

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomerUser
 
 # Register your models here.
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("email", "is_staff", "is_superuser",
+                    "is_courier", "last_login")
+
+
+admin.site.register(CustomerUser, CustomerAdmin)
