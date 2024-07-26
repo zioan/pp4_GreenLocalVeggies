a310990f307f0beac238d4a2d1528a6c9124af67
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-26 15:06:32 +0000

    Return to profile tab after account deletion is cancelled

--- customer/static/customer/javascript/script.js
+++ customer/static/customer/javascript/script.js
@@ -1,7 +1,6 @@
 // Save the active tab in the profile template to sessionStorage.
 // This way, when the page is reloaded, or a form action trigger 
 // a rerender, the active tab will remain active.
-
 document.addEventListener("DOMContentLoaded", function () {
     // Get the saved tab from sessionStorage
     let activeTab = sessionStorage.getItem('activeTab');
@@ -19,4 +18,15 @@ document.addEventListener("DOMContentLoaded", function () {
             sessionStorage.setItem('activeTab', event.target.getAttribute('href'));
         });
     });
+});
+
+// redirect to the profile tab when the cancel button in account delete tab is clicked
+document.getElementById('cancel-account-delete-button')?.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // Set the active tab to "My Profile"
+    sessionStorage.setItem('activeTab', '#v-pills-profile');
+
+    // Redirect to the profile tab
+    window.location.href = event.target.href;
 });
