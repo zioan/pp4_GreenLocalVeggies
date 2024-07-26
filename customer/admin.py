248d780145aa388f227df3f472e5015743e5800d
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-26 15:03:42 +0000

    Exclude unused user fields in admin panel

--- customer/admin.py
+++ customer/admin.py
@@ -8,5 +8,12 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ("email", "is_staff", "is_superuser",
                     "is_courier", "last_login")
 
+    def get_exclude(self, request, obj=None):
+        # Exclude fields from the admin form
+        exclude = ()
+        if obj:
+            exclude = ('password', 'last_login', 'groups', 'user_permissions')
+        return exclude
+
 
 admin.site.register(CustomerUser, CustomerAdmin)
