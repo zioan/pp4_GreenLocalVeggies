e7b4f2f4b3d375378b050674fc18524d9d00d508
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-29 12:06:30 +0000

    Implement pagination and sorting for the shop view

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from .models import Product
 
@@ -7,11 +8,48 @@ from .models import Product
 
 def index(request):
     query = request.GET.get('q')
+    sort = request.GET.get('sort')
+    category = request.GET.get('category')
+    products_list = Product.objects.all()
+
+    # Apply search filter if query exists
     if query:
-        products = Product.objects.filter(name__icontains=query)
-    else:
-        products = Product.objects.all()
-    return render(request, "shop/index.html", {"products": products})
+        products_list = products_list.filter(name__icontains=query)
+
+    # Apply category filter
+    if category:
+        products_list = products_list.filter(category=category)
+
+    # Apply sorting
+    if sort == 'price_asc':
+        products_list = products_list.order_by('price')
+    elif sort == 'price_desc':
+        products_list = products_list.order_by('-price')
+    elif sort == 'name':
+        products_list = products_list.order_by('name')
+
+    # Pagination, 12 products per page
+    paginator = Paginator(products_list, 12)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    # Get all categories for the filter
+    categories = Product.objects.values_list('category', flat=True).distinct()
+
+    context = {
+        "products": products,
+        "categories": categories,
+        "current_sort": sort,
+        "current_category": category,
+        "search_query": query,
+    }
+
+    return render(request, "shop/index.html", context)
 
 
 def product_details(request, product_slug):
