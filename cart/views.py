a931c452443441819477d0e093e73b12f0a017fb
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-30 07:30:35 +0000

    Add cart template

--- cart/views.py
+++ cart/views.py
@@ -26,7 +26,7 @@ def cart_detail(request):
     cart = Cart(request)
     cart_items = cart.get_items()
     print("Cart items:", cart_items)
-    return render(request, "cart/detail.html", {
+    return render(request, "cart/index.html", {
         "cart": cart,
         "cart_items": cart_items
     })
