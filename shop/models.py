260fc6026005a2108c76607ca2a769a16e5b1f8e
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-07-30 07:42:46 +0000

    Fix product model price and stock fields

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 # Create your models here.
 
@@ -17,8 +19,8 @@ class Product(models.Model):
 
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
+    stock = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0'))])
     unit = models.CharField(max_length=10, choices=UNIT_CHOICES)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     image = models.ImageField(upload_to="products/")
