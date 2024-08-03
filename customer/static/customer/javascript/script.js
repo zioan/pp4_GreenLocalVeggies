24c5b89a589bedb7a4731bb1fbcc6e5a7d6dada3
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-08-03 15:08:10 +0000

    Add confirmation modal for account deletion

--- customer/static/customer/javascript/script.js
+++ customer/static/customer/javascript/script.js
@@ -1,9 +1,9 @@
 // Save the active tab in the profile template to sessionStorage.
-// This way, when the page is reloaded, or a form action trigger 
+// This way, when the page is reloaded, or a form action trigger
 // a rerender, the active tab will remain active.
 document.addEventListener("DOMContentLoaded", function () {
     // Get the saved tab from sessionStorage
-    let activeTab = sessionStorage.getItem('activeTab');
+    let activeTab = sessionStorage.getItem("activeTab");
 
     // If there's a saved tab, activate it
     if (activeTab) {
@@ -12,26 +12,41 @@ document.addEventListener("DOMContentLoaded", function () {
             let tab = new bootstrap.Tab(tabElement);
             tab.show();
         } else {
-            return
+            return;
         }
     }
 
     // Add click event listener to all tab-link elements
-    document.querySelectorAll('.tab-link').forEach(function (tab) {
-        tab.addEventListener('click', function (event) {
+    document.querySelectorAll(".tab-link").forEach(function (tab) {
+        tab.addEventListener("click", function (event) {
             // Save the current tab identifier
-            sessionStorage.setItem('activeTab', event.target.getAttribute('href'));
+            sessionStorage.setItem(
+                "activeTab",
+                event.target.getAttribute("href")
+            );
         });
     });
 
     // Redirect to the profile tab when the cancel button in account delete tab is clicked
-    document.getElementById('cancel-account-delete-button')?.addEventListener('click', function(event) {
-        event.preventDefault();
+    document
+        .getElementById("cancel-account-delete-button")
+        ?.addEventListener("click", function (event) {
+            event.preventDefault();
 
-        // Set the active tab to "My Profile"
-        sessionStorage.setItem('activeTab', '#v-pills-profile');
+            // Set the active tab to "My Profile"
+            sessionStorage.setItem("activeTab", "#v-pills-profile");
 
-        // Redirect to the profile tab
-        window.location.href = event.target.href;
+            // Redirect to the profile tab
+            window.location.href = event.target.href;
+        });
+});
+
+// Handle the account deletion confirmation modal
+document.addEventListener("DOMContentLoaded", function () {
+    const deleteForm = document.getElementById("delete-account-form");
+    const confirmDeleteBtn = document.getElementById("confirm-delete");
+
+    confirmDeleteBtn.addEventListener("click", function () {
+        deleteForm.submit();
     });
 });
