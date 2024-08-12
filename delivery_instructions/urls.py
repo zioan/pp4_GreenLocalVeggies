0709d4a8a0b5c3183589fbe1cc0b19abe0896922
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-08-12 20:15:17 +0000

    Remove unused path

--- delivery_instructions/urls.py
+++ delivery_instructions/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.instruction_list, name='instruction_list'),
     path('create/', views.instruction_create, name='instruction_create'),
     path('<int:pk>/update/', views.instruction_update,
          name='instruction_update'
