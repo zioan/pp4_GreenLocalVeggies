f19838630694b62288740b5fd6f5540072d678f4
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-08-16 10:21:01 +0000

    Remove unused customer template

--- customer/urls.py
+++ customer/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
